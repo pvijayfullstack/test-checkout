645594684b08a3eefbb1724ebfdfec212254760f
andistuder <andi@cenatus.org>
2012-03-23 14:54:04 +0000

    checkout system accepts transactions - improved UI

--- spec/views/checkouts/new.html.haml_spec.rb
+++ spec/views/checkouts/new.html.haml_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe "checkouts/new" do
   before(:each) do
     assign(:checkout, stub_model(Checkout).as_new_record)
+
+    assign(:pricing_rules, [
+      FactoryGirl.build(:pricing_rule),
+      FactoryGirl.build(:pricing_rule)
+    ])
   end
 
   it "renders new checkout form" do
