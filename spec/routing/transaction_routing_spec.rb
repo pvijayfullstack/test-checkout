409cde296e04c22d9354fc6a8cab06216a8f9d25
andistuder <andi@cenatus.org>
2012-03-24 19:44:45 +0000

    tidy up routing tests

--- spec/routing/transaction_routing_spec.rb
+++ spec/routing/transaction_routing_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe TransactionsController do
   describe "routing" do
     it "routes to #create" do
-      post("/checkouts").should route_to("checkouts#create")
+      post("/transactions").should route_to("transactions#create")
     end
   end
 end
