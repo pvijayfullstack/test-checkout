3511515ca4de0fa891a029fec52970cdf8b24750
andistuder <andi@cenatus.org>
2012-03-24 19:39:37 +0000

    added test for Checkout#total

--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -9,18 +9,37 @@ describe Checkout do
   it {should have_and_belong_to_many(:pricing_rules)}
 
   describe "Checkout#total" do
-    pending "add some examples to (or delete) #{__FILE__}"
-    #before :each do   #TODO tighten up this test
-    #  @product = FactoryGirl.build(:product, :price => 10)
-    #  @pricing_rule = FactoryGirl.build(:pricing_rule, :discount_type => "bulk", :bulk_price => 8)
-    #  @checkout = FactoryGirl.build(:checkout)
-    #  @transaction = FactoryGirl.build(:transaction, :product => @product, :checkout => @checkout)
-    #  @checkout.transactions << @transaction
-    #end
-    #
-    #it "adds up value of all transactions" do
-    #  @checkout.total.should eq(10)
-    #end
+    before :each do
+      @product1 = FactoryGirl.build(:product, :price => 3)
+      @product2 = FactoryGirl.build(:product, :price => 5)
+    end
+
+    it "adds up checkout transactions" do
+      @checkout = FactoryGirl.build(:checkout)
+      @checkout.transactions << FactoryGirl.build(:transaction, :product => @product1)
+      2.times do
+        @checkout.transactions << FactoryGirl.build(:transaction, :product => @product2)
+      end
+      @checkout.total.should eq(@product1.price+(@product2.price*2))
+    end
+
+    it "recognised a buy one get one free (bogof) pricing rule" do
+      @pricing_rule = FactoryGirl.build(:pricing_rule, :discount_type => "bogof", :product => @product1)
+      @checkout = FactoryGirl.build(:checkout, :pricing_rules => [@pricing_rule])
+      2.times do
+        @checkout.transactions << FactoryGirl.build(:transaction, :product => @product1)
+      end
+      @checkout.total.should eq(@product1.price)
+    end
+
+    it "recognised a bulk pricing rule" do
+      @pricing_rule = FactoryGirl.build(:pricing_rule, :discount_type => "bulk", :product => @product1, :bulk_threshold => 3, :bulk_price => 2.5)
+      @checkout = FactoryGirl.build(:checkout, :pricing_rules => [@pricing_rule])
+      3.times do
+        @checkout.transactions << FactoryGirl.build(:transaction, :product => @product1)
+      end
+      @checkout.total.should eq(@pricing_rule.bulk_price*3)
+    end
   end
 
 end
